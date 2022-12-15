56de2078c3027252c53fa4a85fdd531c3bc20247
POWER\fahad.hussain <fahad.hussain@powersoft19.com>
2022-12-15 18:35:17 +0500

    home page UI added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 const express=require("express")
 const {ROUTE}= require("./routes.js")
+const Book = require('../models/book');
 const router=express.Router()
 
-router.get(ROUTE.Home,(req,res)=>{
-    res.render("index")
+router.get(ROUTE.Home,async(req,res)=>{
+    let books;
+    try {
+        books=await Book.find().sort({createdAt:"desc"}).limit(10).exec()
+    } catch (error) {
+        books=[]
+    }
+    res.render("index",{books:books})
 })
 
 module.exports=router
