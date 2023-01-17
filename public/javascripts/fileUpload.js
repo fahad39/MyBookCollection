4bbdb691b15c0332f3dcd98b26c370133c55a88c
POWER\fahad.hussain <fahad.hussain@powersoft19.com>
2023-01-17 20:20:16 +0500

    completed styles

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,13 +1,26 @@
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileEncode,
-);
+const rootStyles=window.getComputedStyle(document.documentElement)
 
-FilePond.setOptions({
-    stylePanelAspectRatio:1/2,
-    imageResizeTagetWidth:100,
-    imageResizeTagetHeight:150,
-})
+if(rootStyles.getPropertyValue("--book-cover-width-large")!== null && rootStyles.getPropertyValue("--book-cover-width-large")!=="" ){
+    ready()
+}else{
+    document.getElementById("main-css").addEventListener("load",ready)
+}
 
-FilePond.parse(document.body);
+function ready() {
+    const coverWidth=parseFloat(rootStyles.getPropertyValue("--book-cover-width-large"))
+    const coverAspectRatio=parseFloat(rootStyles.getPropertyValue("--book-cover-aspect-ratio"))
+    const coverHeight=coverWidth/coverAspectRatio
+    FilePond.registerPlugin(
+        FilePondPluginImagePreview,
+        FilePondPluginImageResize,
+        FilePondPluginFileEncode,
+    );
+    
+    FilePond.setOptions({
+        stylePanelAspectRatio:1/coverAspectRatio,
+        imageResizeTagetWidth:coverWidth,
+        imageResizeTagetHeight:coverHeight,
+    })
+    
+    FilePond.parse(document.body);
+}
